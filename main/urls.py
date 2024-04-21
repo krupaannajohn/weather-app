26747fde7c001abe56aefcac6e39c1992519d45a
Krupa <krupa.john@bds.christuniversity.in>
2024-04-21 13:21:58 +0530

    commit

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path 
-from . import views 
-  
-urlpatterns = [ 
-         path('', views.index), 
-] 
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),  # Define the URL pattern for the index view
+    path('delete/<int:location_id>/', views.delete_location, name='delete_location'),
+]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+"""from django.shortcuts import render
 
 # Create your views here.
 # import json to load json data to python dictionary 
@@ -46,3 +46,49 @@ def index(request):
         data ={} 
     weather=Weather.objects.all().order_by('-timestamp')
     return render(request, "index.html", data) 
+"""
+
+from django.shortcuts import render, redirect
+import json 
+import urllib.request 
+from .models import Weather
+
+def index(request): 
+    if request.method == 'POST': 
+        city = request.POST['city'] 
+        source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=e8ddef56273a5f305a361716e9246e42').read() 
+        list_of_data = json.loads(source) 
+
+        data = {
+            "name": str(list_of_data["name"]),
+            "country_code": str(list_of_data['sys']['country']), 
+            "coordinate": str(list_of_data['coord']['lon']) + ' '
+                        + str(list_of_data['coord']['lat']), 
+            "temp": str(list_of_data['main']['temp']) + 'k', 
+            "pressure": str(list_of_data['main']['pressure']), 
+            "humidity": str(list_of_data['main']['humidity']), 
+        }
+        # Save weather data to database
+        wdata = Weather(
+            city=str(list_of_data["name"]),
+            country_code=str(list_of_data['sys']['country']),
+            coordinate=str(list_of_data['coord']['lon']) + ' ' + str(list_of_data['coord']['lat']),
+            temp=str(list_of_data['main']['temp']) + 'k',
+            pressure=str(list_of_data['main']['pressure']),
+            humidity=str(list_of_data['main']['humidity'])
+        )
+        wdata.save()
+
+        # Redirect to prevent resubmission on refresh
+        return redirect('index')
+    else: 
+        data = {
+            'bdata': Weather.objects.all().order_by('-timestamp')
+        }
+        
+    return render(request, "index.html", data) 
+
+def delete_location(request, location_id):
+    location = Weather.objects.get(pk=location_id)
+    location.delete()
+    return redirect('index')
